d077cae25fffe36cb246acdce449a2408b00f747
KarlyMakowski <liropache35@gmail.com>
2022-06-28 15:11:41 +0000

    final proyect

--- src/app.js
+++ src/app.js
@@ -5,7 +5,58 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#clic").addEventListener("click", () => {
+    document.querySelector("#laExcusa").innerHTML = generateExcuse();
+  });
+
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let pronombre = ["Un", "El"];
+  let sujeto = [
+    "gato",
+    "perro",
+    "tigre",
+    "conejo",
+    "pajaro",
+    "raton",
+    "mapache"
+  ];
+  let adjetivo = ["salvaje", "peludo", "loco", "asesino", "rabioso"];
+  let accion = [
+    "me ha mordido",
+    "me ha perseguido",
+    "me ha arañado",
+    "me ha aplastado",
+    "me ha atacado"
+  ];
+  let donde = [
+    "por toda la playa",
+    "en un ascensor",
+    "estando en la oficina",
+    "en la cocina",
+    "en una discoteca",
+    "en el hospital"
+  ];
+  let pronombreIndex = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let adjetivoIndex = Math.floor(Math.random() * adjetivo.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let dondeIndex = Math.floor(Math.random() * donde.length);
+
+  return (
+    pronombre[pronombreIndex] +
+    " " +
+    sujeto[sujetoIndex] +
+    " " +
+    adjetivo[adjetivoIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    donde[dondeIndex] +
+    " "
+  );
 };
